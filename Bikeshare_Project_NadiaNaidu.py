72028d6063f997437fd29cf32ef2ef136758aa25
Nadia <naidunadia@gmail.com>
2019-07-19 20:48:03 +0200

    Line 4 docstring explanation added

--- Bikeshare_Project_NadiaNaidu.py
+++ Bikeshare_Project_NadiaNaidu.py
@@ -1,6 +1,8 @@
 import time
 import pandas as pd
 
+#importing modules to be used later in the program.
+
 CITY_DATA = {'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv'}
